27517e51d04f8c8b299afeb0d3e4bbc05c74f707
rsldnc <119888225+rsldnc@users.noreply.github.com>
2023-07-25 21:39:47 +0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdinc <rdinc@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 18:17:45 by rdinc             #+#    #+#             */
+/*   Updated: 2023/07/20 20:28:09 by rdinc            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 15
+# endif
+
+int		find_newline_index(char *buff);
+size_t	ft_strlen(char *str);
+char	*get_next_line(int fd);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdinc <rdinc@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/20 18:18:02 by rdinc             #+#    #+#             */
+/*   Updated: 2023/07/20 18:19:45 by rdinc            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	find_newline_index(char *buff)
+{
+	int	i;
+
+	i = 0;
+	while (buff[i])
+	{
+		if (buff[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
